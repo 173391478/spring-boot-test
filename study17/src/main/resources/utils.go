0cb531993ec90e7247353f3128dabb95c836e702
TheQuietQuotient <The.Quiet.Quotient@hotmail.com>
2025-11-16 13:05:57 +0000

    Add utility function to parse and validate email addresses

---
+++ study17/src/main/resources/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"regexp"
+)
+
+// IsValidEmail checks if the provided string is a valid email address
+func IsValidEmail(email string) bool {
+	// Regular expression for basic email validation
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	match, _ := regexp.MatchString(pattern, email)
+	return match
+}
