bb2062025e7faf53b9cd1b15773516d94f4a2ced
ChromaticCatalyst <chromatic.catalyst@outlook.com>
2025-11-16 13:07:00 +0000

    Add utility function for string manipulation to handle common formatting tasks.

--- study17/src/main/resources/utils.go
+++ study17/src/main/resources/utils.go
@@ -11,3 +11,23 @@ func IsValidEmail(email string) bool {
 	match, _ := regexp.MatchString(pattern, email)
 	return match
 }
+package utils
+
+import (
+	"strings"
+	"unicode"
+)
+
+// CapitalizeWords capitalizes the first letter of each word in the input string.
+// It considers words as sequences of characters separated by spaces.
+func CapitalizeWords(s string) string {
+	words := strings.Fields(s)
+	for i, word := range words {
+		if len(word) > 0 {
+			runes := []rune(word)
+			runes[0] = unicode.ToUpper(runes[0])
+			words[i] = string(runes)
+		}
+	}
+	return strings.Join(words, " ")
+}
